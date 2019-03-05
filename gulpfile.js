5eb36ff58333dc5c707e2a8eb0d62fe3aa0577e7
Nicolas Maas <nicolas.maas@student.howest.be>
2019-03-05 08:52:59 +0100

    Copy of someone else's solution

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// Import van toegevoegde packages (iva npm of yarn)
+
+// De tasks / watchers zelf die we aanmaken
+
+    // We willen een development server opzetten
+
+    // HTML moet geminified naar de dist-map gezet worden
+
+    // JS willen we samenvoegen, minifien en compatibel maken
+
+    // Watchen van veranderingen
+
+// Export van onze eigen tasks (functions)
+// Dan kunnen we ze gebruiken vai de de command line
